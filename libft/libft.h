6636ccca18be2b7adcd178495a8f9fc599e468c2
Alberto Foinquinos <foinquinosalberto@gmail.com>
2024-03-21 15:13:55 +0100

    ft_strlen added

--- libft/libft.h
+++ libft/libft.h
@@ -15,9 +15,10 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *str);
 #endif
