d243cd7467482a484b25e9b60dff0f6ce6afd60a
Alberto Foinquinos <foinquinosalberto@gmail.com>
2024-04-03 11:38:36 +0200

    fix memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -21,7 +21,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	i = 0;
 	c_src = (const char *)src;
 	c_dest = (char *)dest;
-	if (dest == NULL || src == NULL)
+	if (dest == NULL && src == NULL)
 		return (dest);
 	while (i < n)
 	{
