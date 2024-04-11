df6801c33cf2fcb4e8daa655b4434f1c49276fcb
Alberto Foinquinos <foinquinosalberto@gmail.com>
2024-04-11 16:32:16 +0200

    fix strlcat again

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -16,15 +16,17 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	dest_len;
 	size_t	src_len;
+	size_t	count;
 
-	if ((src == NULL || dest == NULL) && (size < 1))
+	if ((src == NULL || dest == NULL) && size < 1)
 		return (0);
 	src_len = ft_strlen(src);
 	dest_len = ft_strlen(dest);
+	count = dest_len;
 	if (size <= dest_len)
 		return (size + src_len);
-	while (*src && (dest_len + 1) < size)
-		dest[dest_len++] = *src++;
-	dest[dest_len] = '\0';
+	while (*src && count < size - 1)
+		dest[count++] = *src++;
+	dest[count] = '\0';
 	return (dest_len + src_len);
 }
