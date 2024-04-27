8b70e3e8cc72413a2ddd46d708be9bd7291e08dd
Alberto Foinquinos <foinquinosalberto@gmail.com>
2024-04-27 12:12:28 +0200

    split started 2

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -12,4 +12,25 @@
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c);
+char	**ft_split(char const *s, char c)
+{
+	char	**split_arr;
+	size_t	c_count;
+	size_t	i;
+
+	c_count = 0;
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+			c_count++;
+		i++;
+	}
+	split_arr = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
+	if (!split_arr)
+		return (NULL);
+	split_arr = ft_write_result(s, c, split_arr);
+	return (split_arr);
+}
