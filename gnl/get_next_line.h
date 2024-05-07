676b7b1c3e5048e3d72e2157a5a39f96f07fedb3
Alberto Foinquinos <foinquinosalberto@gmail.com>
2024-05-07 20:17:05 +0200

    ubuntu commit last changes

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: afoinqui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/02 14:15:30 by afoinqui          #+#    #+#             */
-/*   Updated: 2024/05/02 14:15:32 by afoinqui         ###   ########.fr       */
+/*   Created: 2024/05/03 19:59:38 by afoinqui          #+#    #+#             */
+/*   Updated: 2024/05/03 19:59:42 by afoinqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-char	*get_next_line(int fd)
-{
-    
-}
+# ifndef BUFFER_SIZE 
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
--- gnl/main.c
+++ gnl/main.c
@@ -1,29 +1,38 @@
-#include <stdlib.h>
-#include <fcntl.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afoinqui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/03 20:05:04 by afoinqui          #+#    #+#             */
+/*   Updated: 2024/05/03 20:05:05 by afoinqui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
 #include <stdio.h>
- #include <unistd.h>
+#include <fcntl.h>
 
-#ifndef BUFFER_SIZE 
-# define BUFFER_SIZE 10
-#endif
-
-int main (int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int fd;
-    int n_bytes;
-    char result[BUFFER_SIZE + 1];
+	int		fd;
+	int		n_bytes;
+	char	result[BUFFER_SIZE + 1];
 
-    fd = open (argv[1], O_RDONLY);
-    if (fd == -1)
-        return (printf("fallo"), 1);
-
-    n_bytes = read(fd, result, BUFFER_SIZE);
-
-    
-    printf("Valor de buffer size %d\n", BUFFER_SIZE);
-    printf("Cantidad de bytes leidos %d\n", n_bytes);
-    printf("bufffer leido: [%s]\n", result);
-
-    return (0);
+	if (argc == 2)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+			return (printf("fallo"), 1);
+		n_bytes = read(fd, result, BUFFER_SIZE);
+		printf("Valor de buffer size %d\n", BUFFER_SIZE);
+		printf("Cantidad de bytes leidos %d\n", n_bytes);
+		printf("bufffer leido: [%s]\n", result);
+	}
+	else
+	{
+		printf("Error, por favor envie solo un archivo");
+	}
+	return (0);
 }
-
